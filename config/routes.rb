f60c2bc318af53cfc87fbc392d9260815ca354a4
jeffrothwell <rothwelljeff@gmail.com>
2018-01-18 13:35:53 -0500

    refactor using RESTful routes, fix path_helper in index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pictures#index'
-  get '/pictures' => 'pictures#index'
+
+  resources :pictures
 
   get '/previous_work' => 'pictures#previous_work'
 
   get '/pictures_by_year/:year' => 'pictures#pictures_by_year'
 
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id/edit' => "pictures#edit"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy'
-
-  get 'pictures/:id' => 'pictures#show'
 end
