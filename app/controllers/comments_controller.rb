87e7eaa4e145f8b8a52f256cb24060cf7175a249
jeffrothwell <rothwelljeff@gmail.com>
2018-01-18 20:15:27 -0500

    fix instance variable reference error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @picture = Picture.find(params[:picture_id])
-    @comment = @product.reviews.new
+    @comment = @picture.comments.new
     @comment.message = params[:comment][:message]
 
     if @comment.save
